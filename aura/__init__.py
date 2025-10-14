4e6ac176d232bc9b993e4022723fd6a1bfefd7fd
Hans Trompert <hans.trompert@surf.nl>
2025-10-14 13:54:29 +0200

    rearange code between __main__ and __init__

--- aura/__init__.py
+++ aura/__init__.py
@@ -12,6 +12,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from datetime import UTC, datetime, timedelta
+
+from apscheduler.triggers.interval import IntervalTrigger
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastui import prebuilt_html
+from starlette.responses import HTMLResponse, PlainTextResponse
+
+from aura.frontend.database import router as database_router
+from aura.frontend.home import router as home_router
+from aura.frontend.nsi import router as nsi_router
+from aura.frontend.reservations import router as reservations_router
+from aura.job import nsi_poll_dds_job, scheduler
+from aura.log import init as log_init
+
 #
-# initialise NSI communications
+# logging
 #
+log_init()
+
+#
+# scheduler
+#
+scheduler.start()
+# run poll job every minute starting on the next whole minute and do not let jobs queue up
+scheduler.add_job(
+    nsi_poll_dds_job,
+    trigger=IntervalTrigger(
+        minutes=1, start_date=datetime.now(UTC).replace(second=0, microsecond=0) + timedelta(minutes=1)
+    ),
+    coalesce=True,
+)
+
+#
+# application
+#
+app = FastAPI()
+
+# make sure the folder named 'static' exists in the project,
+# and put the css and js files inside a subfolder called 'assets'
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# include routes
+app.include_router(reservations_router, prefix="/api/reservations")
+app.include_router(database_router, prefix="/api/database")
+app.include_router(nsi_router, prefix="/api/nsi")
+app.include_router(home_router, prefix="/api")
+
+
+@app.get("/robots.txt", response_class=PlainTextResponse)
+async def robots_txt() -> str:
+    return "User-agent: *\nAllow: /"
+
+
+@app.get("/favicon.ico", status_code=404, response_class=PlainTextResponse)
+async def favicon_ico() -> str:
+    return "page not found"
+
+
+@app.get("/{path:path}")
+async def html_landing() -> HTMLResponse:
+    return HTMLResponse(prebuilt_html(title="AURA PoC"))
--- aura/__main__.py
+++ aura/__main__.py
@@ -12,70 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import UTC, datetime, timedelta
+import sys
 
 import uvicorn
-from apscheduler.triggers.interval import IntervalTrigger
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastui import prebuilt_html
-from starlette.responses import HTMLResponse, PlainTextResponse
 
-from aura.frontend.database import router as database_router
-from aura.frontend.home import router as home_router
-from aura.frontend.nsi import router as nsi_router
-from aura.frontend.reservations import router as reservations_router
-from aura.job import nsi_poll_dds_job, scheduler
-from aura.log import init as log_init
+from aura import app
 from aura.settings import settings
 
-#
-# logging
-#
-log_init()
 
-#
-# scheduler
-#
-scheduler.start()
-# run poll job every minute starting on the next whole minute and do not let jobs queue up
-scheduler.add_job(
-    nsi_poll_dds_job,
-    trigger=IntervalTrigger(
-        minutes=1, start_date=datetime.now(UTC).replace(second=0, microsecond=0) + timedelta(minutes=1)
-    ),
-    coalesce=True,
-)
-
-#
-# application
-#
-app = FastAPI()
-
-# make sure the folder named 'static' exists in the project,
-# and put the css and js files inside a subfolder called 'assets'
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# include routes
-app.include_router(reservations_router, prefix="/api/reservations")
-app.include_router(database_router, prefix="/api/database")
-app.include_router(nsi_router, prefix="/api/nsi")
-app.include_router(home_router, prefix="/api")
+def main() -> None:
+    uvicorn.run(app, host=settings.NSI_AURA_HOST, port=settings.NSI_AURA_PORT)
 
 
-@app.get("/robots.txt", response_class=PlainTextResponse)
-async def robots_txt() -> str:
-    return "User-agent: *\nAllow: /"
-
-
-@app.get("/favicon.ico", status_code=404, response_class=PlainTextResponse)
-async def favicon_ico() -> str:
-    return "page not found"
-
-
-@app.get("/{path:path}")
-async def html_landing() -> HTMLResponse:
-    return HTMLResponse(prebuilt_html(title="AURA PoC"))
-
-
-uvicorn.run(app, host=settings.NSI_AURA_HOST, port=settings.NSI_AURA_PORT)
+if __name__ == "__main__":
+    sys.exit(0)
